789c4e5383ee2bbdbc023bf364086fcd2cb48ebd
sunqp <sunqingpeng@hotmail.com>
2015-12-26 13:54:20 +0800

    1. add inner temperature function.

---
+++ Temperature/temperature.h
@@ -0,0 +1,8 @@
+#ifndef _DSTEP_TEMPERATURE_H_
+#define _DSTEP_TEMPERATURE_H_
+
+void Temperature_Config();
+unsigned short Get_Temperature();	
+
+#endif
+
--- main.c
+++ main.c
@@ -8,6 +8,7 @@
 #include "Motor/motor.h"
 #include "Uart/uart.h"
 #include "SPI/spi.h"
+#include "Temperature/Temperature.h"
 
 //free rt os
 #include "FreeRTOS.h"  
@@ -19,11 +20,16 @@ static void vLEDTask( void *pvParameters );
 
 int main()
 {
+	unsigned short adc_value =0;
+	float temperature = 0.0;
 	vTraceInitTraceData();
 	if (! uiTraceStart() ) {
 		vTraceConsoleMessage("Could not start recorder!");
 	}
 	LED_GPIO_Config();
+	Temperature_Config();
+	adc_value = Get_Temperature();
+	temperature= (1.42 - adc_value*3.3/4096)*1000/4.35 + 25;
 	xTaskCreate( vLEDTask, ( const portCHAR * ) "LED", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY+3, NULL );
 	vTaskStartScheduler();  
 }
