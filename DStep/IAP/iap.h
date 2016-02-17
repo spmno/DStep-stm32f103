353aa2744e5dac3ed5ef099c5ed55ddf7d47134f
spmno <sunqingpeng@hotmail.com>
2016-02-17 16:48:54 +0800

    1. add iap directory.

---
+++ DStep/IAP/iap.h
@@ -0,0 +1,24 @@
+#ifndef _DSTEP_IAP_H_
+#define _DSTEP_IAP_H_
+
+#define ApplicationAddress    0x8003000
+
+#if defined (STM32F10X_MD) || defined (STM32F10X_MD_VL)
+ #define PAGE_SIZE                         (0x400)    /* 1 Kbyte */
+ #define FLASH_SIZE                        (0x20000)  /* 128 KBytes */
+#elif defined STM32F10X_CL
+ #define PAGE_SIZE                         (0x800)    /* 2 Kbytes */
+ #define FLASH_SIZE                        (0x40000)  /* 256 KBytes */
+#elif defined STM32F10X_HD || defined (STM32F10X_HD_VL)
+ #define PAGE_SIZE                         (0x800)    /* 2 Kbytes */
+ #define FLASH_SIZE                        (0x80000)  /* 512 KBytes */
+#elif defined STM32F10X_XL
+ #define PAGE_SIZE                         (0x800)    /* 2 Kbytes */
+ #define FLASH_SIZE                        (0x100000) /* 1 MByte */
+#else 
+ #error "Please select first the STM32 device to be used (in stm32f10x.h)"    
+#endif
+
+#define FLASH_IMAGE_SIZE                   (uint32_t) (FLASH_SIZE - (ApplicationAddress - 0x08000000))
+
+#endif
